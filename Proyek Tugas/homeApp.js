d410bb16d8946421d17a372c74fc97624af3eed7
Riyna_267 <rhifannyaqilah02@gmail.com>
2025-02-04 21:21:05 +0800

    Add files via upload

---
+++ Proyek Tugas/homeApp.js
@@ -0,0 +1,103 @@
+const carList = [
+    {
+        carName : "AVANZA",
+        Brand	: "Toyota",
+        Year	: 2019,
+        Price 	: {
+            single : 350000,
+            driver : 500000
+        },
+        imageLink : "images/avanza.webp"
+    },
+    {
+        carName : "INNOVA",
+        Brand	: "Toyota",
+        Year	: 2020,
+        Price 	: {
+            single : 500000,
+            driver : 650000
+        },
+        imageLink : "images/innova.webp"
+    },
+    {
+        carName : "PAJERO",
+        Brand	: "Mitsubishi",
+        Year	: 2018,
+        Price 	: {
+            single : 1300000,
+            driver : 1500000
+        },
+        imageLink : "images/pajero.webp"
+    },
+    {
+        carName : "HILUX",
+        Brand	: "Toyota",
+        Year	: 2018,
+        Price 	: {
+            single : 1300000,
+            driver : 1500000
+        },
+        imageLink : "images/hilux.webp"
+    },
+    {
+        carName : "BRIO",
+        Brand	: "Honda",
+        Year	: 2023,
+        Price 	: {
+            single : 350000,
+            driver : 500000
+        },
+        imageLink : "images/brio.webp"
+    },
+    {
+        carName : "ALPHARD",
+        Brand	: "Toyota",
+        Year	: 2019,
+        Price 	: {
+            single : 4500000,
+            driver : 5000000
+        },
+        imageLink : "images/alphard.webp"
+    },
+    {
+        carName : "TRITON",
+        Brand	: "Mitsubishi",
+        Year	: 2021,
+        Price 	: {
+            single : 1300000,
+            driver : 1500000
+        },
+        imageLink : "images/triton.webp"
+    },
+    {
+        carName : "FORTUNER",
+        Brand	: "Toyota",
+        Year	: 2022,
+        Price 	: {
+            single : 1300000,
+            driver : 1500000
+        },
+        imageLink : "images/fortuner.webp"
+    },
+    {
+        carName : "XPANDER",
+        Brand	: "Mitsubishi",
+        Year	: 2021,
+        Price 	: {
+            single : 400000,
+            driver : 500000
+        },
+        imageLink : "images/xpander.webp"
+    },
+    {
+        carName : "HIACE",
+        Brand	: "Toyota",
+        Year	: 2023,
+        Price 	: {
+            single : 1700000,
+            driver : 2500000
+        },
+        imageLink : "images/hiace.webp"
+    }         
+]
+
